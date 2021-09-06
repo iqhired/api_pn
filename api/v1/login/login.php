d6b240cce25b3d7871301858f646490bf7136dda
Asha M S <ashams001@gmail.com>
2021-09-06 15:17:14 +0530

    API Module

---
+++ api/v1/login/login.php
@@ -0,0 +1,55 @@
+<?php
+require "../../../vendor/autoload.php";
+use \Firebase\JWT\JWT;
+
+include_once '../../../config/database.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../../../classes/v1/sgUsers.php';
+
+
+$jwt = $_SERVER['HTTP_ACCESS_TOKEN'];
+if($jwt){
+	try {
+
+		$decoded = JWT::decode($jwt, $secretkey, array('HS256'));
+
+		// Access is granted. Add code of the operation here
+
+		$database = new Database();
+		$db = $database->getConnection();
+
+		$item = new SgUsers($db);
+
+		$data = json_decode(file_get_contents("php://input"));
+
+		$item->user_name = $_POST['user'];
+		$item->password = $_POST['password'];
+
+		$sgUser = $item->getUserByUNameandPassword();
+		if($sgUser != null){
+			http_response_code(200);
+			echo json_encode(array("STATUS" => "Success" , "user" => $sgUser));
+		} else{
+
+			http_response_code(401);
+			echo json_encode(array("message" => "Login failed. Check User Name and Password"));
+		}
+
+	}catch (Exception $e){
+
+		http_response_code(401);
+
+		echo json_encode(array(
+			"message" => "Access denied.",
+			"error" => $e->getMessage()
+		));
+	}
+
+}
+?>
---
+++ classes/v1/sgUsers.php
@@ -0,0 +1,71 @@
+<?php
+class SgUsers{
+
+	// Connection
+	private $conn;
+
+	// Table
+	private $db_table = "cam_users";
+
+	// Columns
+	public $users_id;
+	public $user_name;
+	public $mobile;
+	public $email;
+	public $password;
+	public $role;
+	public $profile_pic;
+	public $created_at;
+	public $updated_at;
+	public $assigned;
+	public $assigned2;
+	public $firstname;
+	public $lastname;
+	public $hiring_date;
+	public $total_days;
+	public $job_title_description;
+	public $job_title_id;
+	public $shift_location;
+	public $shift_location_id;
+	public $available;
+	public $available_time;
+	public $pin;
+	public $pin_flag;
+	public $training;
+	public $training_station;
+	public $training_position;
+
+	// Db connection
+	public function __construct($db)
+	{
+		$this->conn = $db;
+	}
+
+	public function getUserByUNameandPassword(){
+
+		$sqlQuery= "SELECT * FROM ". $this->db_table ." WHERE user_name= ? and password = ?";
+
+		$stmt = $this->conn->prepare($sqlQuery);
+
+		$stmt->bindParam(1, $this->user_name);
+		$stmt->bindParam(2, $this->password);
+
+		$stmt->execute();
+
+		$dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+		if($dataRow == null || empty($dataRow)){
+			return null;
+		}else{
+			$this->users_id = $dataRow['users_id'];
+			$this->available = $dataRow['available'];
+			$this->user_name = $dataRow['user_name'];
+			$this->role = $dataRow['role'];
+			$this->profile_pic = $dataRow['profile_pic'];
+			$this->firstname = $dataRow['firstname'];
+			$this->lastname = $dataRow['lastname'];
+			$this->pin = $dataRow['pin'];
+			$this->pin_flag = $dataRow['pin_flag'];
+			return $this;
+		}
+	}
+}
