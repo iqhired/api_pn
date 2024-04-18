0831af464332287de5994ebc37e28535d78f39d6
Darshan V <darshan.v@iqhired.com>
2024-04-18 11:48:46 +0530

    redirect api is done but need to verify user auth

--- api/v1/redirect-to-iotcise/redirect.php
+++ api/v1/redirect-to-iotcise/redirect.php
@@ -1,17 +1,23 @@
 <?php
-require "../../../vendor/autoload.php";
-use Firebase\JWT\JWT;
+
+require '../../../vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+
+// secret key
+$key = "aseqw5844q734aqqweFADFd54GE456A";
+
+// Payload data
+$payload = array(
+    "credentials" => 'test.admin',
+    "username" => "example_user",
+    "exp" => time()+1000
+);
 
 
-    $secretkey = "SupportPassHTSSgmmi";
-    $payload = array(
-        'credentials' => $_GET['username'],
-        "author" => "Saargummi to HTS",
-        "exp" => time()+1000
-    );
-    $jwt = JWT::encode($payload, $secretkey, 'HS256');
-    $externalDomain = "http://127.0.0.1:8000/external-auth";
-    $redirectUrl = $externalDomain . "?token=" . $jwt;
-    header("Location: $redirectUrl");
-    echo $jwt;
+$jwt = JWT::encode($payload, $key, 'HS256');
+$externalDomain = "http://127.0.0.1:8000/external-auth";
+$redirectUrl = $externalDomain . "?token=" . $jwt;
+header("Location: $redirectUrl");
+
 ?>
